895e347d55a390e1d5158395a57d6e0e9472cd3f
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-19 16:55:49 -0300

    Update LinkedList.js

--- 5 hours/LinkedList.js
+++ 5 hours/LinkedList.js
@@ -10,3 +10,10 @@ aquele elemento que deixou de ser apontado, é garbage collected e se vai
 
 muuuuito mais barato que deletar algo em um array, q depois tem que trocar todas as coisas de lugar na memoria
 
+push() --> grab the tail, point it to the new element. then change the 'tail' assignment
+
+para fazer o pop(), da ate para keep track of the penultimate tail. ai deleta o que vem dps...
+
+orr (and?) you have to make the second to last (the penultimate tail?) point to nothing (null), then it becomes the tail
+     
+keep track of the head to know the list. tail is even optional
