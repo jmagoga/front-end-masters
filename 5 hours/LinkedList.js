f98332f2f235d4c764626ffc44f78d126218bbfc
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-19 20:00:40 -0300

    Update LinkedList.js

--- 5 hours/LinkedList.js
+++ 5 hours/LinkedList.js
@@ -25,26 +25,59 @@ class LinkedList {
     this.tail = null
   }
   push(value) {
-    
+    const node = new Node(value)
+    this.length++
+    if (!this.head) {
+      this.head = node //statement bem interessante. tudo em uma linha
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node
   }
-  pop() {
-    
-    return value
+  pop(value) {
+    return this.delete(this.length-2)
   }
   _find(value, test=this._test) {
-    
+    let current = this.head
+    let i = 0
+    while(current) {
+      if (test(value, current.value, i, current)) { //current.value = whatever node I am looking at. current is the node itself
+        return current
+      }
+        current = current.next
+        i++
+    }
+    return null // I didn't get what I was looking for
   }
   _test(a, b) {
-    return a === b;J
+    return a === b;
   }
   _testIndex(search, __, i) { //'__' means I don't care what this is, as I'm not referencing it later
     return search === i //did I find the correct index?
   }
   get(index) {
-    
+    const node = this._find(index, this._testIndex)
+    if (!node) return null;
+    return node.value
   }
   delete(index) {
-    
+    if (index === 0) {
+      const head = this.head;
+      if (head) {
+        this.head = head.next //para não deletar de cara a head, ela tem que apontar para algum lugar
+      } else {
+        this.tail = this.head = null
+      }
+      this.length--
+      return head.value
+    }
+    const node = this._find(index-1, this._testIndex) //procurando pela coisa que está antes
+    const excise = node.next //excise deleting
+    if (!excise) return null //se nao tem nada para deletar, então não deleta
+    node.next = excise.next //grab the node that is going to be delted, grab its next and make it the previous node's next
+    if (node.next && !node.next.next) this.tail = node.next//2 hops
+    this.length--
+    return excise.value
   }
 }
 
