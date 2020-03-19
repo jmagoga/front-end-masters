973c105b3779d29eb01227dc550cd8bdc4017ad6
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-19 11:30:21 -0300

    Update basicRecursion.js

--- 5 hours/basicRecursion.js
+++ 5 hours/basicRecursion.js
@@ -1,4 +1,5 @@
 function basicRecursion(max, current) {
+    //base case (whne you stop recursing)
   if (current > max) return //e.g quando for (current 6, max 5)
   console.log(current)
   basicRecursion(max, current+1) //chama de novo aumentando o current
