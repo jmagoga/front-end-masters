16ed7d4f53617489a2b9422e13af62ecd9dd03ad
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-19 15:38:08 -0300

    Create arrayList.js

---
+++ 5 hours/arrayList.js
@@ -0,0 +1,49 @@
+imagina que tu tem um array
+
+['foo', 'bar', 'baz', 'what', 'ever'] e tu deleta 'bar' (indice 1). todos os elementos da direita tem que mover
+                                      para a esquerda um local (pensa nisso na memoria (cs50 malan), 
+                                      cada espacinho indo 1x para o lado)
+                                      
+ou se tu vai adicionar um elemento ali antes de 'bar', tu tem que mover tudo para a direita 1x. isso é bem caro!!!
+nos queremos que elas sejam rapidas
+
+lookups is fine. goes straight to whereer it needs to go
+
+
+OPTIMIZED for gets, but DEOPTIMZED for deletes and inserts
+
+class ArrayList {
+  constructor() {
+    this.length = 0;
+    this.data = {}
+  }
+  push(item) {
+    this.data[this.length] = item
+    this.length++
+  }
+  pop() {
+    const lastItem = this.data[this.length-1]
+    delete this.data[this.length-1]
+    this.length--
+    return lastItem
+  }
+  get(index) {
+    return this.data[index]
+  }
+  delete(index) {
+    const ans = this.data[index]
+    _collapse(index)
+    return ans
+  }
+  _collapse(index){
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i+1]
+    }
+    delete this.data[this.length-1]
+    this.length--
+  }
+}
+
+
+
+
