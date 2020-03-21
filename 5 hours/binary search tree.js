a0442b6acd89caad0ce179329515970c0029ecd2
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-21 14:16:09 -0300

    Update binary search tree.js

--- 5 hours/binary search tree.js
+++ 5 hours/binary search tree.js
@@ -26,3 +26,50 @@ lookups are bigO(log n)... to add a sortedList to a BST you can have bigO(n)
 (if you have a sortedList inserted, it's going to be a straight line in a sortedList)
 
 having the data randomized is very important for a binary search tree
+
+class Tree {
+  constructor() {
+    this.root = null
+  }
+  toObject() {
+    return this.root
+  }
+  add(value) {
+    if (this.root === null) {
+      //no head, create new head
+      this.root = new Node(value);
+      return;
+    }
+    
+    let current = this.root //this is the node we are CURRENTLY looking at
+    while(true) { //ele vai acontecer quantas vezes precisarmos... até que a gente break ou return
+      if (current.value > value) { //this node i'm inspecting atm, is your value grater than the value I am trying to find? then go left!!
+        //go left
+        if (current.left) { // if I have a left child, run the same process with my left child AS current
+          current = current.left
+        }
+        else { //if I do not have a left child, you have found the spot you're supposed to insert this, and you're done!!! (faz isso criando um novo Node)
+          current.left = new Node(value)
+          return;
+        }
+      } else {
+        //go right
+        if (current.right) {
+          current = current.right;
+        } 
+        else {
+          current.right = new Node(value);
+          return;
+        }
+      }
+    }
+  }
+}
+
+class Node {
+  constructor(value, left=null, right=null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
