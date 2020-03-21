c84d65238af545819a8f7ac02b91b9a327e19ba0
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-21 17:03:12 -0300

    Create AVL Tree.js

---
+++ 5 hours/AVL Tree.js
@@ -0,0 +1,16 @@
+it's a binary search tree but not vice versa
+
+lesser on left, more on right
+
+it exists to keep trees as flat as possible, it's the entire point of an AVL. it makes sure that
+
+'sorted trees' dont happen
+
+has to be recursive. ask if children are in balance --> só ver que uma tree q tenha um branch maior q o outro (em um nivel a mais)
+
+AVL tree é uma tree que é mais organizada, q nao tenha um lado mto maior que o outro
+
+worst case ALWAYS BigO(log n)
+
+AVL existe pra mitigar o worst case sceario de uma BST(binary search tree)
+
