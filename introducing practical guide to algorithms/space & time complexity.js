05e682bcd3e9b36fdbca63e1250c40eb99149b27
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-23 18:31:22 -0300

    Update space & time complexity.js

--- introducing practical guide to algorithms/space & time complexity.js
+++ introducing practical guide to algorithms/space & time complexity.js
@@ -1,1 +1,16 @@
-first
+SLOW (n^2) (quadratic) if we compare all the numbers to one another (nested loops)
+(n) (linear) if you're looping over all of the numbers once (single loop)
+(2n) (linear) if we run through all the numbers two times (you do two loops, separately, each which goes over all of the nums)
+(2) if we only do 2 operations despite the number of input (goes directly to the source, if you have to do two operations, find min and max, e.g.)
+FAST (1) (constant) one operation, tipo o find index 0
+
+
+FAST (log n) logarithmic (geralmente divide and coquer ????) 
+SLOW (k^n) exponential
+
+(n log n) -- linear * logarithmic
+
+
+
+ordem de time complexity
+(1), (log n), (ao quadrado N), (n), (n^2), (n^3), (n^n)
