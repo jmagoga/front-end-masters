621bb8fabb5c8b329d908a066110f9d81a4805a1
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-23 20:35:20 -0300

    Update uniqueSort.js

--- introducing practical guide to algorithms/uniqueSort.js
+++ introducing practical guide to algorithms/uniqueSort.js
@@ -1,7 +1,7 @@
 const uniqSort = (arr) => {
   const breadcrumbs = {}
-  let result = [arr[0]]
-  for(let i=1; i < arr.length; i++) {
+  let result = []
+  for(let i=0; i < arr.length; i++) {
     if (!breadcrumbs[arr[i]]) {
       result.push(arr[i])
       breadcrumbs[arr[i]] = true
@@ -10,7 +10,8 @@ const uniqSort = (arr) => {
   return result.sort((a, b) => a - b)
 }
 
-console.log(uniqSort([1,3,1, 2]))
+console.log(uniqSort([1,3,1, 2])) //1, 2, 3
+
 
 //conseguimos fazer em vez de O(n^2) quadratic, O(n), linear, usando caching com o breadcrumb. mas...
 //problem with space complexity we need to consider (toda vez vamos cacheando mais e mais coisas na memoria)
