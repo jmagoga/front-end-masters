108cb6d979791820a5c996a4ecc9985a76966df6
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-24 08:32:55 -0300

    Create recursion.js

---
+++ introducing practical guide to algorithms/recursion.js
@@ -0,0 +1,23 @@
+let tracker = 0
+const callMe = () => {
+  tracker++
+  if (tracker === 3) {
+    return 'loops'
+  }
+  return callMe('anytime') //RETORNA o 'loops' lá da função qnd esta no stack com tracker === 3
+}
+
+console.log(callMe())
+
+//sem o return statement temos problemas, pq não temos o valor daquela função
+
+let tracker = 0
+const callMe = () => {
+  tracker++
+  if (tracker === 3) {
+    return 'loops'
+  }
+  callMe('anytime') //WRONG. implicit return that returns 'undefined'. We should stick a 'return' in front of it.
+}
+
+console.log(callMe())
