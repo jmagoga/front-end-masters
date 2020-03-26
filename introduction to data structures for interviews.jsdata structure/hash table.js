06f4f31ef5ecbceda74e14abec658ea5227baeec
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-26 14:02:07 -0300

    Create hash table.js

---
+++ introduction to data structures for interviews.jsdata structure/hash table.js
@@ -0,0 +1,33 @@
+hash table is not ordered 
+
+good for fast lookups and keys that are flexible
+
+
+they organize data for quick look ups on value for given keys
+
+translate keys into a memory address (bcs we need to go directly there)
+
+like, I just point here... and there's the value. I don't need to loop through all of them
+
+this is constant time look up!
+
+now it's just gonna be the index of an array. under the hood it could be an address in memory
+
+they're used in security... passwords...
+
+once it's half full, you double it
+
+para ficar em um alcance e 25 (range), apenas mod o número por 25!!! 
+ex, 100 % 25 --> 0
+99 % 25 --> 24
+98 % 25 --> 23
+
+
+
+
+
+
+
+
+
+
