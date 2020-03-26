5b429215468edabd40f31db8b8c481f07fb057a4
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-26 15:48:05 -0300

    Update Stack.js

--- introduction to data structures for interviews.jsdata structure/Stack.js
+++ introduction to data structures for interviews.jsdata structure/Stack.js
@@ -8,15 +8,24 @@ class Stack {
     this._length++
   }
   pop() {
-    const last = this._storage[this._length-1]
-    this._storage[this._length-1] = undefined //same as deleting
-    //delete this._storage[this._length-1]   //usar o 'delete' eh praticamente o mesmo que setar para undefined
-    this._length--
-    return last
+    if (this._length) {
+      const last = this._storage[this._length-1]
+      this._storage[this._length-1] = undefined //same as deleting
+      //delete this._storage[this._length-1]   //usar o 'delete' eh praticamente o mesmo que setar para undefined
+      this._length--
+      return last
+    } else {
+      //throw an error? how would you want this implemented // or just return undefined as an array so that API's are similar (API, how you interact with it)
+    }
     
   }
-  peek(){
-  
+  peek(){ //always peek that last one
+    if (this._length) {
+      return this._storage[this.length-1]
+    } else {
+      return undefined
+    }
+    
   }
 }
 
