2f6a1bba6c41f3a14d66b655ddacf929f8405259
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-26 12:32:43 -0300

    Update data structures.js

--- introduction to data structures for interviews.jsdata structure/data structures.js
+++ introduction to data structures for interviews.jsdata structure/data structures.js
@@ -1,1 +1,27 @@
-1
+reverse a linked list
+
+keep track of a min value in a stack in constant time
+
+how can I reason about this problem based on the data structures I know
+
+data strcuture is an arragement of data, how you interact with this data and how it's displayed in memory
+
+array - uma do lado da outra na memoria
+ordered data structs
+hierarchical
+complex relationships
+
+search, sort, get particular values... do that all the time, but in different scenarios
+
+DOM is a tree, traversing the DOM all the time.
+
+how traversing something like a tree works
+
+DATA STRUCTURE            USED FOR
+arrays, strings            ordered data, words
+hash tables               optimization   --> typically lookup
+linked lists              data insert/ delete   --> build other data stuctures, like an array (under the hood)
+stacks/queues             creative aux DS    --> keep track of things as an aux ds
+trees/ heaps              hierarchical data 
+graphs                    complex relationships
+
