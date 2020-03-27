5c8c15b9b4ac9863addcd2bba43a3d805124da12
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-27 13:05:25 -0300

    Update linked list exercise.js

--- introduction to data structures for interviews.jsdata structure/linked list exercise.js
+++ introduction to data structures for interviews.jsdata structure/linked list exercise.js
@@ -1,1 +1,34 @@
-1
+class LinkedList {
+  constructor(value) {
+    this.head = {value, next: null} //same as saying 'value: value', shorthand.
+    this.tail = this.head //they point to the same object in memory
+  }
+  insert(value) {
+    //update head and tail as needed. only update head if it's null
+    const node = {value, next: null} //cria um novo node
+    this.tail.next = node //primeiro, pega o next do atual tail e poe esse novo node como next
+    this.tail = node //transforma o tail no node (agora é esse, o último que foi adicionado)
+  }
+  remove() {
+    
+  }
+  removeTail() {
+    
+  }
+  contains() { //linear
+    
+  }
+  isHead() { //constant time
+    
+  }
+  isTail() { //constant time
+    
+  }
+}
+
+const list = new LinkedList(1) //not taking any value (nothing in the constructor) //should I initiate it with a value or empty
+console.log(list)
+list.insert(2)
+console.log(list)
+list.insert(3)
+console.log(list)
