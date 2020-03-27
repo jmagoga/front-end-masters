c56be5db975dcffcbe7e811f06f170ba04322b80
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-27 14:37:06 -0300

    Update hash table exercise.js

--- introduction to data structures for interviews.jsdata structure/hash table exercise.js
+++ introduction to data structures for interviews.jsdata structure/hash table exercise.js
@@ -1,1 +1,23 @@
-1
+class HashTable {
+  constructor() {
+    this._storage = []
+  }
+  insert() { //size... handle collision
+
+  }
+  remove() {
+    
+  }
+  retrieve() {
+    
+  }
+  _hash(str, n) {
+    let sum = 0
+    for (let i = 0; i < str.length; i++) {
+      sum += str.charCodeAt(i) * 3
+    }
+    
+    return sum % n
+  }
+  
+} 
