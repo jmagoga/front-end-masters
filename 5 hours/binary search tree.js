9ed1756fea8c192e0064982ad4f7313e10beced0
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-30 10:44:40 -0300

    Update binary search tree.js

--- 5 hours/binary search tree.js
+++ 5 hours/binary search tree.js
@@ -76,3 +76,70 @@ class Node {
     this.right = right;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//revisao
+
+class Tree {
+  constructor() {
+    this.root = null
+  }
+  toObject() {
+    return this.root  
+  }
+  add(value) {
+    if (this.root === null) {
+      this.root = new Node(value);
+      return
+    }
+    let current = this.root
+    while(true) {
+      if(current.value > value) {
+        if (current.left) {
+          current = current.left
+        } else {
+          current.left = new Node(value);
+          return //tem que sair desse loop otherwise eterno
+        } 
+      } 
+      else { //não esquecer esse else aqui
+        if (current.right) {
+          current = current.right
+        } else {
+          current.right = new Node(value)
+          return
+        }
+       }
+      }      
+    }
+}
+
+class Node {
+  constructor(value, left=null, right=null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+const tree = new Tree
+console.log(tree)
+tree.add(13)
+console.log(tree)
+tree.add(2)
+console.log(tree)
+tree.add(25)
+console.log(tree)
+tree.add(55)
+console.log(tree)
