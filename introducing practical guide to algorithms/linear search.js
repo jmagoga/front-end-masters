10a323059f8991d25f8f4e74441e8a826de84615
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-03-30 14:17:01 -0300

    Update linear search.js

--- introducing practical guide to algorithms/linear search.js
+++ introducing practical guide to algorithms/linear search.js
@@ -9,3 +9,16 @@ const linearSearch = (arr, number) => {
 }
 
 console.log(linearSearch([1, 2, 3], 2)) //1 -- index onde está o 2. 
+
+
+
+//REVISAO
+
+function linearSearch(list, num) { //assumption: list is already sorted
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === num) return i
+  }
+  return -1
+}
+
+console.log(linearSearch([1, 2, 3, 4, 11, 15, 16 ,17 ,18], 6))
