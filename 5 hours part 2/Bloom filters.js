371c09b8ba140826952d0ffd2a71dd0d8d229511
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-05 16:59:37 -0300

    Update Bloom filters.js

--- 5 hours part 2/Bloom filters.js
+++ 5 hours part 2/Bloom filters.js
@@ -56,6 +56,20 @@ hashFunc2 = 1
 //in Medium, if Shirley is an article, it'll be shown that the article might has been read. therefore, not shown to user.
 
 
-
+//EXERCISE
+  
+//we need an internal data structure to keep track of this. we use an array
+  
+class BloomFilter { //no need for the constructor
+  _array = new Array(100).fill(0)  //class properties. creates something on the instance of this particular bloom filter, which his _array (private). no futuro vai ter # (#array) que faz ele de fato privado
+  add(string) {
+   this._array[h1(string)] = 1 //h1 é um hash function (pegar qqr uma em algum lugar)
+   this._array[h2(string)] = 1 //em todas essas, se flipa o 0 pra 1, pra dizer q está dentro do array em tais lugares e dps comparar
+   this._array[h3(string)] = 1 //poe o 1 no index que essa função retornar
+  }
+  contains(string) {
+    return !!(this._array[h1(string)] && this._array[h2(string)] && this._array[h3(string)]) //string coercion (vira true or false) se todos estão neste local
+  }
+}
 
 
