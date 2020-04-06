a4d0f9fe9bf254d11c180aa2c4b1a79e4b80e1c6
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-05 22:43:12 -0300

    Update Tree Traversal.js

--- 5 hours part 2/Tree Traversal.js
+++ 5 hours part 2/Tree Traversal.js
@@ -1,2 +1,17 @@
 //Depth first traversal
 
+//if you want a sorted list out of a binary search tree (BST) then you use an inorder traversal
+
+//to create a deep copy, use a pre ordered traversal
+
+const preorderTraverse = (node, array) => {
+  
+}
+
+const inorderTraverse = (node, array) => {
+  
+}
+
+const postorderTraverse = (node, array) => {
+  
+}
