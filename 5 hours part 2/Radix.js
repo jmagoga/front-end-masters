469763da983f4776789a9321aa08b488711b7f4d
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-11 08:55:45 -0300

    Update Radix.js

--- 5 hours part 2/Radix.js
+++ 5 hours part 2/Radix.js
@@ -5,3 +5,7 @@ sort baseado nos ńumeros tipo, 1, dezena, centena...
 134 -- 134 --  99
  99 -- 235 -- 134
 235 --  99 -- 235
+
+não se compara um número a outro. simplesmente se poe ele em 'baldes'
+
+o balde do 0, do, 1, o balde do 2... até o 9... depois faz sort com base no radical.
