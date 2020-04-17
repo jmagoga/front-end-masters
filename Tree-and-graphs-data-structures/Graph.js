7b442ee3fdd63e0ef05daa3b174402247b00e4ca
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-17 09:46:30 -0300

    Update Graph.js

--- Tree-and-graphs-data-structures/Graph.js
+++ Tree-and-graphs-data-structures/Graph.js
@@ -1,1 +1,11 @@
-1
+//pensa nas conexoes em forma de matriz
+
+//AJACENCY MATRIX
+
+-    1 2 3 4 5
+
+1    0 1 0 0 1       --> 1 conecta com 2 e 5
+2    1 0 1 1 1       -->
+3    0 1 0 1 0       -->
+4    0 1 1 0 1       -->
+5    1 1 0 1 0       -->
