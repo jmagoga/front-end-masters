635f28b155a182d258a838afff2871cf5396636e
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-23 14:59:43 -0300

    Update Effects.js

--- Complete Intro to React v5/Effects.js
+++ Complete Intro to React v5/Effects.js
@@ -6,3 +6,10 @@ tudo é renderizado antes, o que tem dentro do useEffect é renderizado depois.
 
 usado para nao demorar para mostrar tudo na tela. se tu deixa aquela chamada pra API sem o useEffect, ele nao vai renderizar nada
 até que aquele chamada é resolvida. por isso, se schedule essa chamada pra ser depois que a pagina inteira ja renderizou
+
+
+
+
+****se o useEffect não tem as dependencias (do que ele depende), ele vai rodar toda vez que da render.***
+  pensar no quando queremos que aconteça a mudança de dentro do useEFfect, quando o que mudar? e assim escolher as dependencias
+  que gerealmente sao coisas que estao dentro do useEFfect mesmo.
