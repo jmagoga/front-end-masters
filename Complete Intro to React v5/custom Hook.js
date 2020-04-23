237cb4e99f638a65f244705f66ef47670a1e3fa8
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-23 14:44:55 -0300

    Create custom Hook.js

---
+++ Complete Intro to React v5/custom Hook.js
@@ -0,0 +1,45 @@
+const useDropDown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState)
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`
+  
+  const Dropdown = () => {
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={e => setState(e.target.value)}
+        onBlur={e => setState(e.target.value)}
+        disabled={!options.length}
+      >
+        <option>All</option>
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+        )}
+      </select>
+    </label>
+  );
+  }
+  return [state, Dropdown, setState]
+}
+
+
+
+
+
+
+
+import useDropdown from '/useDropdown'
+
+const [animal, animalDropdown] = useDropdown("Animal", "dog", ANIMALS)
+const [breed, breedDropdown] = useDropdown("Breed", "", breeds)
+
+
+
+
+
+<AnimalDropdown />
+<BreedDropdown />
