536d6d8d154a6519bc2248197c02212ac9c33488
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-24 11:49:30 -0300

    Update CSS in JS.js

--- Intermediate React v2/CSS in JS.js
+++ Intermediate React v2/CSS in JS.js
@@ -1,13 +1,20 @@
-library emotion.js https://emotion.sh/docs/introduction
+//library emotion.js https://emotion.sh/docs/introduction
 
-extensao que ajuda vscode-styled-components
+//extensao que ajuda vscode-styled-components
+
+//you can manipulate styles more programatically. a lot of control!!! difficult to do in separate documents for css(scss)
 
 import {css} from "@emotion/core"
 
+const color = 'pink'
+const [padding, setPadding] = useState(15)
+
 <header
-  css={css` //backticks
-    background-color: #333;
-    padding: 15px;
+  onClick={() => setPadding(padding + 15)}
+//backticks
+  css={css` 
+    background-color: ${color};            //já que é uma template string!!!
+    padding: ${padding}px;
   `}
 > 
   
