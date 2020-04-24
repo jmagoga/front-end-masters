51577c98a811e969e809fcbd36c049bebebea074
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-24 10:29:21 -0300

    Update hooks.js

--- Intermediate React v2/hooks.js
+++ Intermediate React v2/hooks.js
@@ -1,2 +1,9 @@
 //https://btholt.github.io/complete-intro-to-react-v5/
 
+//cor dentro do componente
+
+<h1 onClick={() => setIsGreen(!isGreen)}
+    style={{color: isGreen ? 'green' : 'crimson'}}
+> 
+  Texto
+</h1>
