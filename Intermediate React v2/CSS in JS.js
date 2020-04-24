e06663a287845bf1418d9c161eebf7f93aa5bd2b
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-24 11:39:35 -0300

    Create CSS in JS.js

---
+++ Intermediate React v2/CSS in JS.js
@@ -0,0 +1,14 @@
+library emotion.js https://emotion.sh/docs/introduction
+
+extensao que ajuda vscode-styled-components
+
+import {css} from "@emotion/core"
+
+<header
+  css={css` //backticks
+    background-color: #333;
+    padding: 15px;
+  `}
+> 
+  
+</header>
