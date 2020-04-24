5cc9063c54605a800ec2968c534917eb805b05f0
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-24 09:35:35 -0300

    Update and rename first.js to async await.js

---
+++ JavaScript the Recent Parts/async await.js
@@ -0,0 +1,14 @@
+//await só se usa dentro de um async. entao nao da pra por um await dentro de um método por exemplo, forEach,
+//pq ele recebe uma função e o await estaria dentro dessa função que náo é async.
+
+async function fetchFiles(files) {
+  var prs = files.map (getFile)
+  prs.forEach( function each(pr) {
+    console.log(await pr) //nao vai funcionar. ta dentro da function 'each' q nao eh asyncrona.
+  })
+}
+
+//em vez do forEach
+for (let pr of prs) { // não é uma função, eh um simples loop, por isso da pra ter o await ali dentro
+  console.log(await pr)
+}
