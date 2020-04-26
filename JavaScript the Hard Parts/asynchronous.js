60c0d63b4e8b521ea5d56ec1f209ff91a7034ffb
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-26 17:07:18 -0300

    Update asynchronous.js

--- JavaScript the Hard Parts/asynchronous.js
+++ JavaScript the Hard Parts/asynchronous.js
@@ -1,1 +1,7 @@
-1
+ //not from javascript but from the browser
+
+//network request - fetch() / xhr is a network request,
+//local storage - local storage
+//timer - settimeout
+//htmldom - document
+//console console()
