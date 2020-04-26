280b77e9bc96190c5cd174c78d1b3803edcd3e6c
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-26 09:59:53 -0300

    Update first.js

--- JavaScript the Hard Parts/first.js
+++ JavaScript the Hard Parts/first.js
@@ -1,1 +1,17 @@
-1
+//quase mesma coisa q implementar map
+function copyArrayAndManipulate(arr, instruction) {
+  const output = []
+  for (let i = 0; i < arr.length; i++) {
+    output.push(instructions(arr[i]))
+  }
+  return output
+}
+
+//ai temos outra função
+const multiplyBy2 = input = input * 2
+
+//obviamente podemos passar ela como callback
+copyArrayAndManipulate([1, 2, 3], multiplyBy2)
+
+//mas tb nao precisamos definir a função!!!
+copyArrayAndManipulate([1, 2, 3], input => input * 2)
