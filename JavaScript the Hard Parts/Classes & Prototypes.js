bf8346cce7ddef69b57e4c1170cb8e1c2660b96f
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-27 10:43:36 -0300

    Update Classes & Prototypes.js

--- JavaScript the Hard Parts/Classes & Prototypes.js
+++ JavaScript the Hard Parts/Classes & Prototypes.js
@@ -14,9 +14,15 @@ user2.name = "Jack"
 user2.score = 3
 user2.increment = function() {user2.score++}
 
+//or
 
+const user3 = Object.create(null)
 
+user3.name = "Eva"
+user3.score = 3
+user3.increment = function() {user2.score++}
 
+//
 
 
 
