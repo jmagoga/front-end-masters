300a38636053e4347ae5a06036c95883ba935f8c
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-27 10:50:27 -0300

    Update Classes & Prototypes.js

--- JavaScript the Hard Parts/Classes & Prototypes.js
+++ JavaScript the Hard Parts/Classes & Prototypes.js
@@ -22,7 +22,18 @@ user3.name = "Eva"
 user3.score = 3
 user3.increment = function() {user2.score++}
 
+//create and return objects using a function
+
+function userCreator(name, score) {
+  const newUser = {}
+  newUser.name = name
+  newUser.score = score
+  newUser.increment = function() {newUser.score++}
+  return newUser
+}
+
+const user1 = userCreator("Will", 9)
+const user2 = userCreator("Joan", 4)
+user1.increment()
+
 //
-
-
-
