5c98de042e1d5e9ee834d18e029fe8411844847d
jmagoga <43731907+jmagoga@users.noreply.github.com>
2020-04-29 15:30:27 -0300

    Update first.js

--- Firebase/first.js
+++ Firebase/first.js
@@ -5,3 +5,32 @@ useEffect(() => {
     }
     getSnapshot()
   }, [])
+
+
+//primeiro useEffect
+useEffect(() => {
+    async function getSnapshot() {
+      const snapshot = await firestore.collection('businesses').get()
+      snapshot.forEach(doc => {
+      const id = doc.id
+      const data = doc.data()
+
+      console.log({ id, data })
+      })
+    }
+    getSnapshot()
+  }, [])
+
+//segundo, já conseguindo recuperar dados da base de dados
+useEffect(() => {
+    async function getSnapshot() {
+      const snapshot = await firestore.collection('businesses').get()
+      // a lot of 'businesses' objects
+      const businessesFromDatabase = snapshot.docs.map(doc => { 
+        return { id: doc.id, ...doc.data()}
+      })
+                            //passando como um objeto. da para ser sem tb
+      setBusinessesFromDatabase( businessesFromDatabase )
+    }
+    getSnapshot()
+  }, [])
